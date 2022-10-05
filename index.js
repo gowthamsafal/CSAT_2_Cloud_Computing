4f50fc003061aa1113fe052c79c1d4855cf5a773
gowthamsafal <102951180+gowthamsafal@users.noreply.github.com>
2022-10-04 23:11:52 -0400

    node server and package.json

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Gowtham safal");
+const express = require('express');
+const server = express();
+const port = 8080;
+server.get('/',(req,res)=>{
+    res.send("<h1>GOWTHAM SAFAL!!!</h1>");
+    res.end();
+});
+
+server.get('/Home',(req,res)=>{
+    res.send("<h1>Home</h1>");
+    res.end();
+});
+
+server.listen(process.env.port|| port,()=>{
+    console.log("Server running");
+});
+
+
